6cecd827a3668d4bdacece846bb40b0b9aa1d473
Manac3 <mehmetanac3@gmail.com>
2022-09-25 13:37:36 +0300

    Add project files.

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace VirtualMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlDatabase sqldb = new SqlDatabase();
+            sqldb.Update();
+        }
+    }
+
+    abstract class Database //base,parent abstract ile soyutladık
+                            //0D: Dependency Inversion aşağı yöne genişlesin demek
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added by database. ");
+        }
+        public virtual void Update() //bu metod override edilebilir (yani ezilebilir)
+        {
+            Console.WriteLine("Updated by database. ");
+        }
+    }
+    class SqlDatabase : Database
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Updated by sql database. ");
+            base.Update();
+        }
+    }
+    class OracleDatabase : Database
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Updated by oracle database. ");
+            base.Update();
+        }
+    }
+}
+//bu projede virtual ethod öğrendik yani kalıtımda bi önceki kalıtımı değiştirebilmek
+//için kullanıyoruz virtual yaptığımız method daha sonraki classda override
+//ile değiştirilebilir
+
+//ayrıca abstract class ile clası newlemeyi öğreniyoruz
+//    sadece classın başına abstract yazıoyruz bitiyor
---
+++ İnheritance/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace Inheritance1
+{
+    internal class Program //interfaceler abstracttır yani newlenemez ama classlar concrete(somut) dir newlenir
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Mehmet",
+                Surname = "Anaç"
+            };
+            person.DisplayName();
+
+            Student student = new Student()
+            {
+                Department = "Biology",
+                Name = "Mehmet",
+                Surname = "Anaç"
+            };
+            student.DisplayName();
+
+            Person insan = new Customer()
+            {
+                CardNo = "**** **** **** **22",
+                Name = "Seda",
+                Surname = "Anac"
+            };
+            insan.DisplayName();
+            Console.WriteLine(((Customer)insan).CardNo); //casting: tip dönüşüm
+            Console.WriteLine((insan as Customer).CardNo);
+
+            Person[] people = new Person[3]
+            {
+                new Student()
+                {
+                    Department = "Yazılım",
+                    Name = "Ali",
+                    Surname ="anaç"
+                },
+                new Customer()
+                {
+                    CardNo = "**** **12",
+                    Name = "Zeynep",
+                    Surname = "Uzun"
+                },
+                new ExtendedCustomer()
+                {
+                    Address = "Ankara",
+                    Name = "Mehmet",
+                    Surname = "Anaç",
+                    CardNo = "**** **45"
+                }
+            };
+            foreach (Person p in people)
+            {
+                p.DisplayName();
+            }
+
+            ExtendedCustomer extended = (people[2] as ExtendedCustomer);
+            extended.DisplayName();
+            Console.WriteLine(extended.CardNo + " - " + extended.Address);
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public void DisplayName()
+        {
+            Console.WriteLine($"{Name} {Surname}");
+        }
+    }
+    class Student : Person // bunun adı kalıtım demektir //sub, child
+    {
+        public string Department { get; set; }
+    }
+    class Customer : Person //is a relationship // sub, child
+    {
+        public string CardNo { get; set; }
+    }
+    sealed class ExtendedCustomer : Customer, IAddress //mühürlendi bu class artık miras alınamaz bu classtan
+    {
+        public string Address { get; set; }
+    }
+
+    interface IAddress
+    {
+        string Address { get; set; }
+    }
+}
